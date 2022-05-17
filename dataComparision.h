a6466366d0d146cb83def30684f440c28c6dede3
obsessed <abishesshr@gmail.com>
2022-05-18 00:29:29 +0545

    added const int errorNames

--- dataComparision.h
+++ dataComparision.h
@@ -4,6 +4,15 @@
 #define SPACE 32
 using namespace std;
 
+const int error_data =  0;
+const int error_register= 1;
+const int error_space= 2;
+const int error_H= 3;
+const int error_instructionSize= 4;
+const int error_flag= 5;
+const int error_registerPair=6;
+const int error_memoryExceed= 7;
+
 int charToInt(char);
 char intToChar(int);
 int dataStringToInt(string);
